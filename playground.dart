aee8ef086589679d7a5813ee144fda2f1550b3c6
Yamil <yoms.tf@gmail.com>
2023-09-05 22:57:39 +0100

    starting doing things

--- playground.dart
+++ playground.dart
@@ -1,3 +1,12 @@
+//import "dart:core";
+import "dart:io"; //To use input-output
+
 main(){
-  print("Hi mom!");
+  var firstName= "Yamil"; //variable that can have any type
+  String lastName = "Melian"; // variable with a type
+  print(firstName + " " + lastName);
+
+  stdout.writeln("What is your name?");//get input from user
+  String? name= stdin.readLineSync();//read input from user and store in variable
+  print("The user name is $name");
 }
