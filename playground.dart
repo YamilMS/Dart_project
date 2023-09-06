407e961197ec2f89c622cf7d706049b02952d99e
Yamil <yoms.tf@gmail.com>
2023-09-06 11:58:31 +0100

    fundamentals

--- playground.dart
+++ playground.dart
@@ -9,4 +9,20 @@ main(){
   stdout.writeln("What is your name?");//get input from user
   String? name= stdin.readLineSync();//read input from user and store in variable
   print("The user name is $name");
+
+  String literalString =r"This string is a raw string so you would see this: \n";
+  String multiLine= """This string
+                    will have
+                    multiples lines""";
+  print(literalString + " " + multiLine);
+  
+  int one =int.parse("1");
+  double onePointOne= double.parse("1.1");
+  print(one);
+  print(onePointOne);
+
+  int number= 1;
+  String numberStr= number.toString();
+  print(numberStr);
+  
 }
